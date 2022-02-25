eda8aa8ac2b349dc0c1f460d3a5844abedb765a1
billydean <billy.goehring@gmail.com>
2022-02-25 14:00:57 -0800

    finished pizza schema

--- src/validation/pizzaSchema.js
+++ src/validation/pizzaSchema.js
@@ -7,11 +7,38 @@ const pizzaSchema = yup.object().shape({
         .required('Name is required')
         .min(2, 'name must be at least 2 characters'),
     sauce: yup
-        .string
+        .string()
         .oneOf(['white','marinara','pesto','bbq'],'Must select a sauce'),
     size: yup
-        .number
-        .oneOf([11,14,17],'Must select a size')
+        .number()
+        .oneOf([11,14,17],'Must select a size'),
+    pepperoni: yup
+        .boolean(),
+    mushrooms: yup
+        .boolean(),
+    onions: yup
+        .boolean(),
+    sausage: yup
+        .boolean(),
+    bacon: yup
+        .boolean(),
+    olives: yup
+        .boolean(),
+    bellpeppers: yup
+        .boolean(),
+    pineapple: yup
+        .boolean(),
+    spinach: yup
+        .boolean(),
+    morecheese: yup
+        .boolean(),
+    special: yup
+        .string()
+        .optional(),
+    glutensub: yup
+        .boolean(),
+    quantity: yup
+        .number()
 })
 
 export default pizzaSchema;
